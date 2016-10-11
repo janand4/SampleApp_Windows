1544c3769d73e7f6ca69c3a09330c4aa8e0727dd
Janisha Anand <anand.janisha@gmail.com>
2016-10-11 18:08:57 -0400

    Update after-install.bat

--- after-install.bat
+++ after-install.bat
@@ -1,4 +1,2 @@
 
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Set-ExecutionPolicy Unrestricted
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module SQLPS
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Invoke-Sqlcmd -ServerInstance 107.23.132.144 -Database DMSSourceDB -Query "select * from dbo.Customer" -username awssct -password Password1
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Set-ExecutionPolicy Unrestricted;Import-Module SQLPS;Invoke-Sqlcmd -ServerInstance 107.23.132.144 -Database DMSSourceDB -Query "select * from dbo.Customer" -username awssct -password Password1
