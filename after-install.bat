7addc9397826674decb1029e6c92dffba2dafe26
Janisha Anand <anand.janisha@gmail.com>
2016-10-11 18:39:57 -0400

    Update after-install.bat

--- after-install.bat
+++ after-install.bat
@@ -1,2 +1,2 @@
 
-c:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command Set-ExecutionPolicy Unrestricted;Import-Module SQLPS;Invoke-Sqlcmd -ServerInstance 107.23.132.144 -Database DMSSourceDB -Query "select * from dbo.Customer" -username awssct -password Password1
+c:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command Set-ExecutionPolicy Unrestricted;"& {Add-PSSnapin SqlServerCmdletSnapin100; Add-PSSnapin SqlServerProviderSnapin100;invoke-sqlcmd -ServerInstance 107.23.132.144 -Database DMSSourceDB -Query "select * from dbo.Customer -username awssct -password Password1"}"
