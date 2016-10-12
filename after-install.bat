14599ab5e68788b68b942fe1dc10a6cafcc21ac2
Janisha Anand <anand.janisha@gmail.com>
2016-10-12 11:10:45 -0400

    Update after-install.bat

--- after-install.bat
+++ after-install.bat
@@ -1,3 +1,3 @@
-
-$MyArray = "MyVar1 = 'awssct'", "MyVar2 = 'Password1'"  
+FOR /F %i IN (https://s3.amazonaws.com/teamcity-demo1/username-password.txt) DO SET VAR=%i
+$MyArray = "MyVar1 = '$1'", "MyVar2 = '$2'"  
 c:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command Set-ExecutionPolicy Unrestricted;"& {Add-PSSnapin SqlServerCmdletSnapin100; Add-PSSnapin SqlServerProviderSnapin100;invoke-sqlcmd -ServerInstance 107.23.132.144 -Database DMSSourceDB -Query "select * from dbo.Customer -username $(MyVar1) -password $(MyVar2)" -Variable $MyArray  }"
